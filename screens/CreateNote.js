31417a3f79dcca07b2d37a5227f9c3d10a45a021
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2022-10-03 17:56:38 +0300

    added function to save note(assync storage)

--- screens/CreateNote.js
+++ screens/CreateNote.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View,Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { ApplicationProvider, BottomNavigation, BottomNavigationTab, Layout, Text } from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
+import { useState } from 'react/cjs/react.production.min';
+import AsyncStorage from '@react-native-community/async-storage';
 
 
-export default function App() {
+export default function createNote() {
+    const [note,setNote] = useState("")
+    const navigation = useNavigation()
+
+    const saveNote = async () => {
+        const value = await AsyncStorage.getItem("NOTES")
+        const n = value ? JSON.parse(value) : []
+        n.push(note)
+        await AsyncStorage.AsyncStorage.setIem("NOTES", JSON.stringify(n)).then(() => navigation.navigate("AllNotes"))
+    }
   return (
  
     <View>
-        <Text>createNote</Text>
+        <Text>create Note</Text>
     </View>
   );
 }
